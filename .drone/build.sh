82a2efe053392f74b7ed1b956ed2d380e9e445c8
jacekelgda <jacekelgda@gmail.com>
2015-07-10 15:09:24 +0200

    build docker image

--- .drone/build.sh
+++ .drone/build.sh
@@ -2,8 +2,10 @@
 set -e
 cd /var/cache/drone/src/github.com/jacekelgda/drone-test
 
+docker login -e="." -u="jacekelgda+coreosdeploy" -p="$AUTH" quay.io
+
 wrapdocker &
 sleep 5
 
-docker build -t docker-registry/image-name .
-docker push docker-registry/image-name
+docker build -t quay.io/jacekelgda/drone-test:init .
+docker push quay.io/jacekelgda/drone-test:init
